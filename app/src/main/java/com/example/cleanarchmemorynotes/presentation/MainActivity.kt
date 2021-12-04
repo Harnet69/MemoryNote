4545621f03b2e6d32ef1c9b7cc0278d753b22c4a
Vital Kokhanau <vital.kokhanau@mobica.com>
2021-12-04 19:47:46 +0100

    Add dependencies

---
+++ app/src/main/java/com/example/cleanarchmemorynotes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cleanarchmemorynotes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cleanarchmemorynotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
