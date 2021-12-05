bf44f6f5fea16aa00fdfa04a4b04dc016294ea94
Vital Kokhanau <vital.kokhanau@mobica.com>
2021-12-05 16:45:07 +0100

    Implement Navigation

--- app/src/main/java/com/vikk/cleanarchmemorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/vikk/cleanarchmemorynotes/presentation/NoteFragment.kt
@@ -1,19 +1,36 @@
 package com.vikk.cleanarchmemorynotes.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cleanarchmemorynotes.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.cleanarchmemorynotes.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveNoteFab.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/vikk/cleanarchmemorynotes/presentation/NotesListFragment.kt
+++ app/src/main/java/com/vikk/cleanarchmemorynotes/presentation/NotesListFragment.kt
@@ -1,18 +1,39 @@
 package com.vikk.cleanarchmemorynotes.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cleanarchmemorynotes.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.cleanarchmemorynotes.databinding.FragmentNotesListBinding
 
 class NotesListFragment : Fragment() {
+
+    private var _binding: FragmentNotesListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    ): View {
+        _binding = FragmentNotesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addNoteFab.setOnClickListener {
+            goToNoteDetails()
+        }
+    }
+
+    private fun goToNoteDetails(noteId: Long = 0L){
+        findNavController().navigate(NotesListFragmentDirections.actionNotesListFragmentToNoteFragment(noteId))
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
